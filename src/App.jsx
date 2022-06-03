2fa105aa6d38e34e3da69712a75a3c012a415a4c
baptiste geffroy <baptistegeffroy@macbook-pro-de-baptiste.home>
2022-06-03 13:41:19 +0200

    Update: add a router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
